739f4bba64f0cc99c749e18e1b09f8ad371f1c97
chen_xiaowei <965957349@qq.com>
2020-09-17 19:52:17 +0800

    master

--- gitTest/src/main/java/com/chenx/UserTest.java
+++ gitTest/src/main/java/com/chenx/UserTest.java
@@ -7,6 +7,8 @@ public class UserTest {
         System.out.println("dev change");
         System.out.println("dev");
         System.out.println("master");
-        System.out.println("online change 2");    
+        System.out.println("online change");
+        System.out.println("master change");
+        System.out.println("online change 2");
     }
 }
